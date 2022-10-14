4660d87819696214997ba78f2913cdcee91f54dc
toran777 <robertmilos7@gmail.com>
2022-10-14 14:59:57 +0200

    fix progressbar

---
+++ public/test.js
@@ -0,0 +1,22 @@
+const words = "decline ignore great ostrich piano torch whip scorpion actor hard path riot ancient sleep zero dial present insane vivid embark combine pulse latin tuition";
+
+onmessage = (message) => {
+    let iteration = 0;
+    const allcombo = 24 * 24;
+    let possword, poss_seed, seedword;
+    const wordslist = words.split(' ');
+    const seed_list = message.data.seed.split(' ');
+    for (let i = 0; i < wordslist.length; i++) {
+        possword = wordslist[i];
+        for (let j = 0; j < seed_list.length; j++) {
+            iteration += 1;
+            console.log(iteration);
+            postMessage({percentage: Math.floor(iteration * 100 / allcombo)});
+            seedword = seed_list[j];
+            poss_seed = message.data.seed.replace(seedword, possword);
+            postMessage({seed: poss_seed});
+        }
+    }
+
+    postMessage({result: false});
+}
--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react';
-import './style.css';
+import React, { useState, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
-import { one_error_allwords } from './test.js';
 import Modal from 'react-bootstrap/Modal';
 import Overlay from 'react-bootstrap/Overlay';
 import Tooltip from 'react-bootstrap/Tooltip';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import { MnemonicKey, LCDClient } from '@terra-money/terra.js';
 
 
 const App = () => {
-  const words = "decline ignore great ostrich piano torch whip scorpion actor hard path riot ancient sleep zero dial present insane vivid embark combine pulse latin tuition";
   const [width, setWidth] = useState("");
   const [width1, setWidth1] = useState("");
   const [height, setHeight] = useState("");
@@ -19,103 +16,137 @@ const App = () => {
   const [address, setAddress] = useState("");
   const [seed, setSeed] = useState("");
   const [show, setShow] = useState(false);
-  const [percentage, setPercentage] = useState(0);
-  const handleClose = () => {
-    setShow(false);
-    setProgress(-1);
-  };
-  const handleClick = () => {
-    one_error_allwords(seed, words, address, (iteration) => {setPercentage(iteration);}).then((response) => {
-      setResult(response);
-      setShow(true);
-      console.log(response);
-    })
-    loadProgressBar();
-  }
-
-function loadProgressBar() {
-  setInterval(function () {
-    let val = percentage;
-    console.log(val);
-    if (val >= 100) {
-      val = 100;
-      clearInterval();
-    }
-
-    setProgress(val)
-  }, 1000);
-}
-
   const [result, setResult] = useState("");
   const [showtool, setShowtool] = useState(false);
   const [progress, setProgress] = useState(-1);
   const target = useRef(null);
 
+  const handleClose = () => {
+    setShow(false);
+    setProgress(-1);
+  };
+
+  const handleClick = () => {
+    const worker = new Worker('test.js');
+    const params = {seed: seed, address: address};
+    worker.postMessage(params);
+    worker.onmessage = (message) => {
+      const r = message.data;
+      if (r.percentage >= 0) setProgress(r.percentage);
+      else if (r.seed && checkseed(r.seed, address)) {
+        setProgress(100);
+        worker.terminate();
+        setShow(true);
+        setResult(r.seed);
+      } else if (r.result === false) {
+        setProgress(100);
+        worker.terminate();
+        setShow(true);
+        //TODO spawnare un dialogo diverso dove si fa vedere l'errore
+        setResult('Complexity too high');
+      }
+    };
+  }
+
   function copytxt(result) {  
     navigator.clipboard.writeText(result).then();
   }
 
+  function checkseed(seed, address, blockchain = "Terra"){
+    let chain;
+
+    if (blockchain === "Terra"){
+      chain = new LCDClient("https://pisco-lcd.terra.dev/", "pisco-1");
+    } else {
+      //TODO implementare altre chain
+      return false;
+    }
+
+    const key = new MnemonicKey({mnemonic: seed});
+    const wallet = chain.wallet(key);
+    const possaddress = wallet.key.accAddress;
+
+    return possaddress === address;
+  }
+
   return(
     <div className="App">
-    <Container className="center">
-    <p>Recover your Cryptø</p>
-    <><input id = "address" type = "text" 
-        onChange={(event) => setAddress(event.target.value)}
-        onBlur={(event) => {
-          if (event.target.value === '') {
-            setWidth('300px');
-            setHeight('30px');
-          } else {
+      <Container className="center">
+        <p>Recover your Cryptø</p>
+        <input type="text"
+          placeholder="Enter address..."
+          style={{ width: width, height: height }}
+          onChange={(event) => setAddress(event.target.value)}
+          onBlur={(event) => {
+            if (event.target.value === '') {
+              setWidth('300px');
+              setHeight('30px');
+            } else {
+              setHeight('45px');
+            }
+          }}
+          onFocus={() => {
+            setWidth('500px');
             setHeight('45px');
-          }
-        }}
-        onFocus={() => { setWidth('500px'); setHeight('45px'); }} style={{ width: width, height: height }}
-        placeholder="Enter address..." />
-        <input className="custom-input" id = "seed" type = "text"
+          }} />
+        <input className="custom-input"
+          type="text"
+          placeholder="Enter seed phrase..."
+          style={{ width: width1, height: height1 }}
           onChange={(event) => setSeed(event.target.value)}
           onBlur={(event) => {
             if (event.target.value === '') {
-            setWidth1('300px');
+              setWidth1('300px');
               setHeight1('30px');
             } else {
               setHeight1('45px');
             }
-           }}
-        onFocus={() => { setWidth1('100%'); setHeight1('45px'); }} style={{ width: width1, height: height1 }}
-        placeholder="Enter seed phrase..." /></>
-    <Button style={{marginTop: 15}} class="pt-5" variant="success" size="lg" disabled={!address || !seed}
-      onClick={handleClick}>
-      Run
-    </Button>
-    <ProgressBar animated now={progress} variant="primary" style={{marginTop: 30, height: 20, width: 700, backgroundColor: "grey"}}/>
-    <Modal
+          }}
+          onFocus={() => {
+            setWidth1('100%');
+            setHeight1('45px');
+          }} />
+        <Button variant="success"
+          size="lg"
+          disabled={!address || !seed}
+          style={{marginTop: 15}}
+          onClick={handleClick}>Run
+        </Button>
+        {
+          progress > -1 ? <ProgressBar animated
+            now={progress}
+            variant="primary"
+            style={{marginTop: 30, height: 20, width: 700, backgroundColor: "grey"}}>
+          </ProgressBar> : <div></div>
+        }
+      </Container>
+      <Modal
         show={show}
         onHide={handleClose}
         backdrop="static"
-        keyboard={false}
-      >
-      <Modal.Header closeButton>
-      <Modal.Title id="contained-modal-title-vcenter">
-        This should be your seedphrase! 😎
-      </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {result}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button ref={target} variant="success" onClick={() => { copytxt(result); setShowtool(!showtool);}}>
-          Copy seedphrase
-        </Button>
-        <Overlay target={target.current} show={showtool} placement="top">
-          {(props) => (
-          <Tooltip id="overlay-example" {...props}>
-            Copied!
-          </Tooltip>
-          )}
-      </Overlay>
-      </Modal.Footer>
+        keyboard={false}>
+        <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          This should be your seedphrase! <span role="img" aria-label="emoji">😎</span>
+        </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {result}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button ref={target}
+            variant="success"
+            onClick={() => { copytxt(result); setShowtool(!showtool);}}>Copy seedphrase
+          </Button>
+          <Overlay target={target.current} show={showtool} placement="top">
+            {(props) => (
+              <Tooltip id="overlay-example" {...props}>
+                Copied!
+              </Tooltip>
+            )}
+          </Overlay>
+        </Modal.Footer>
       </Modal>
-    </Container>
     </div>
   );
 }
